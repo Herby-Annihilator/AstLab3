8bdefa9af2e09c994da525cea8ebb61f8a09ff47
Rukin Danil <Rukin2018D@yandex.ru>
2021-10-19 20:15:27 +0700

    Окно для отображения графика Ганта

---
+++ Models/Services/UserInformatorGanttChart.cs
@@ -0,0 +1,16 @@
+﻿using AstLab3.Models.Schedules;
+using AstLab3.Models.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AstLab3.Models.Services
+{
+	public class UserInformatorGanttChart : IUserInformator<NetworkSchedule>
+	{
+		public void Show(NetworkSchedule instance)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
